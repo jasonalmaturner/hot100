b6b38e8f519fcd2079c8fca9a1f9c39b5f40db8d
Jason Turner <jasonalmaturner@gmail.com>
2016-02-09 22:43:55 -0700

    rename file

--- server-assets/models/usersModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  accessToken: { type: String },
-  dateCreated: { type: Date },
-  email: { type: String, index: true },
-  
-})
